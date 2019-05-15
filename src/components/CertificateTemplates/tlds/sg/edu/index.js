72cf68715c946b6287be8604e1fe661901aeea7e
hyhongyue <hyho@suss.edu.sg>
2019-05-15 11:23:40 +0800

    fix code style issue

--- src/components/CertificateTemplates/tlds/sg/edu/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/index.js
@@ -4,4 +4,8 @@ import np from "./np";
 import singaporetech from "./singaporetech";
 import suss from "./suss";
 
-export default addDirToTemplatePath("edu", { ...np, ...singaporetech, ...suss });
+export default addDirToTemplatePath("edu", {
+  ...np,
+  ...singaporetech,
+  ...suss
+});
--- src/components/CertificateTemplates/tlds/sg/edu/suss/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/suss/index.js
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 
 const sussFullTime2019 = dynamic(
-	import("./2019-SUSS-FT-Template" /* webpackChunkName: "suss-Templates" */)
+  import("./2019-SUSS-FT-Template" /* webpackChunkName: "suss-Templates" */)
 );
 
 export default {
