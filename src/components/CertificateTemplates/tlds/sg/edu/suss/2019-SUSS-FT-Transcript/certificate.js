ca4dbf127bd74a22e328357c2b755454bfbe8997
hyhongyue <hyho@suss.edu.sg>
2019-06-19 13:53:01 +0800

    transcript is dynamic

--- src/components/CertificateTemplates/tlds/sg/edu/suss/2019-SUSS-FT-Transcript/certificate.js
+++ src/components/CertificateTemplates/tlds/sg/edu/suss/2019-SUSS-FT-Transcript/certificate.js
@@ -1,193 +1,217 @@
 import PropTypes from "prop-types";
 import { get } from "lodash";
-import { SUSS_LOGO, registrarSampleSign } from "./images";
+import { format } from "date-fns";
+import { SUSS_LOGO } from "./images";
+
+const engLocale = require("date-fns/locale/en");
 
 const borderStyle = {
-  border: 5,
-  borderColor: "#8C9093",
-  borderStyle: "solid"
+	border: 5,
+	borderColor: "#8C9093",
+	borderStyle: "solid"
 };
 
 const logoImgStyle = {
-  width: "30%",
-  height: "40%"
+	width: "100%",
+	height: "auto",
+	marginLeft: "15px"
 };
 
 const officialTransStyle = {
-  fontFamily: "Montserrat",
-  fontSize: "17pt",
-  fontWeight: "bold",
-  textAlign: "center",
-  color: "#003B5C"
+	fontFamily: "Montserrat-Medium",
+	fontSize: "17pt",
+	fontWeight: "bold",
+	textAlign: "center",
+	color: "#003B5C"
 };
 
 const bodyCopyStyle = {
-  fontFamily: "Montserrat",
-  fontSize: "9pt"
+	fontFamily: "Montserrat",
+	fontSize: "9pt"
 };
 
 const centerTransData = {
-  textAlign: "center"
+	textAlign: "center"
 };
 
 const boldLabels = {
-  fontWeight: "bold"
+	fontWeight: "bold"
 };
 
 const Template = ({ certificate }) => {
-  const transcriptData = get(certificate, "transcript", []);
+	// Declare what will be needed in the Transcript
 
-  // Rendering an array of transcript data
-  const transcriptSection = transcriptData.map((t, i) => (
-    <tr key={i}>
-      <td>{t.year}</td>
-      <td style={centerTransData}>{t.semester}</td>
-      <td>{t.courseCode}</td>
-      <td>{t.name}</td>
-      <td style={centerTransData}>{t.creditUnits}</td>
-      <td style={centerTransData}>{t.result}</td>
-      <td style={centerTransData}>{t.gradePoint}</td>
-    </tr>
-  ));
+	const transcriptData = get(certificate, "transcript", []);
 
-  return (
-    <div className="container" style={borderStyle}>
-      <div className="row justify-content-center">
-        <img src={SUSS_LOGO} style={logoImgStyle} />
-      </div>
-      <div className="row justify-content-center">
-        <p style={officialTransStyle}>OFFICIAL TRANSCRIPT</p>
-      </div>
+	// Rendering an array of transcript data
+	const transcriptSection = transcriptData.map((t, i) => (
+		<tr key={i}>
+			<td>{t.year}</td>
+			<td style={centerTransData}>{t.semester}</td>
+			<td>{t.courseCode}</td>
+			<td>{t.name}</td>
+			<td style={centerTransData}>{t.creditUnits}</td>
+			<td style={centerTransData}>{t.result}</td>
+			<td style={centerTransData}>{t.gradePoint}</td>
+		</tr>
+	));
 
-      <div className="row">
-        <div className="col">
-          <p style={bodyCopyStyle}>
-            NG WEI XUAN (Huang WEIXUAN) <br />
-            BLK 324 TAMPINES STREET 32 <br />
-            #01-353 <br />
-            SINGAPORE 460206 <br />
+	return (
+		<div className="container" style={borderStyle}>
+			<div className="row">
+				<div className="col-4" />
+
+				<div className="col-4">
+					<img src={SUSS_LOGO} style={logoImgStyle} />
+				</div>
+
+				<div className="col-4" />
+			</div>
+
+			<div className="row">
+				<div className="col-3" />
+
+				<div className="col-6">
+					<p style={officialTransStyle}>OFFICIAL TRANSCRIPT</p>
+				</div>
+
+				<div className="col-3" />
+			</div>
+
+			<div className="row">
+				<div className="col">
+					{certificate.recipient.name.toUpperCase()} <br />
+					{certificate.recipient.address.toUpperCase()} <br />
+					{certificate.recipient.unitNo} <br />
+					SINGAPORE {certificate.recipient.postalCode} <br />
+				</div>
+			</div>
+
+			<br />
+
+			<div className="row">
+				<div className="col">
+					Name <br />
+					<br />
+					Personal Identifier
+        </div>
+				<div className="col">
+					: {certificate.recipient.name.toUpperCase()} <br />
+					<br />: {certificate.recipient.personalIdentifier}
+				</div>
+				<div className="col">
+					NRIC/FIN/PP No. <br />
+					Date of Issue <br />
+					Page Count <br />
+				</div>
+				<div className="col">
+					: {certificate.recipient.nric} <br />:{" "}
+					{format(certificate.issuedOn, "DD MMMM YYYY", { locale: engLocale })}{" "}
+					<br />
+					: Page 1 of 2 <br />
+				</div>
+			</div>
+
+			<br />
+
+			<div className="row">
+				<div className="col">
+					<p>
+						This transcript confirms that the student named above has
+						succesfully completed the listed courses leading to the
+						qualification shown below. For an explanation of the terms and
+						symbols used, please see the other side of this trascript.
           </p>
-        </div>
-      </div>
+				</div>
+			</div>
 
-      <div className="row">
-        <div className="col">
-          Name <br />
-          <br />
-          Personal Identifier
-        </div>
-        <div className="col">
-          : NG WEI XUAN (HUANG WEIXUAN) <br />
-          <br />: E1571349
-        </div>
-        <div className="col">
-          NRIC/FIN/PP No. <br />
-          Date of Issue <br />
-          Page Count <br />
-        </div>
-        <div className="col">
-          : S9416304H <br />
-          : 25 March 2019 <br />
-          : Page 1 of 2 <br />
-        </div>
-      </div>
+			{transcriptData !== [] && (
+				<div className="row">
+					<div className="col">
+						<table className="w-100">
+							<tbody>
+								<tr>
+									<th>Year</th>
+									<th style={centerTransData}>Semester</th>
+									<th>Course</th>
+									<th>Title</th>
+									<th style={centerTransData}>Credit Units</th>
+									<th style={centerTransData}>Result</th>
+									<th style={centerTransData}>Grade Point</th>
+								</tr>
+								{transcriptSection}
 
-      <br />
+								<tr>
+									<td style={boldLabels} colSpan="4">
+										Total number of credit units counted towards this award:{" "}
+									</td>
+									<td style={centerTransData}>
+										<b>{certificate.additionalData.totalCreditUnits}</b>
+									</td>
+								</tr>
 
-      <div className="row">
-        <div className="col">
-          <p>
-            This transcript confirms that the student named above has
-            succesfully completed the listed courses leading to the
-            qualification shown below. For an explanation of the terms and
-            symbols used, please see the other side of this trascript.
-          </p>
-        </div>
-      </div>
+								<tr>
+									<td style={boldLabels} colSpan="6">
+										Culmulative Grade Point Average:{" "}
+									</td>
+									<td style={centerTransData}>
+										<b>{certificate.additionalData.cgpa}</b>
+									</td>
+								</tr>
 
-      {transcriptData !== [] && (
-        <div className="row">
-          <div className="col">
-            <table className="w-100">
-              <tbody>
-                <tr>
-                  <th>Year</th>
-                  <th style={centerTransData}>Semester</th>
-                  <th>Course</th>
-                  <th>Title</th>
-                  <th style={centerTransData}>Credit Units</th>
-                  <th style={centerTransData}>Result</th>
-                  <th style={centerTransData}>Grade Point</th>
-                </tr>
-                {transcriptSection}
-
-                <tr>
-                  <td style={boldLabels} colSpan="4">
-                    Total number of credit units counted towards this award:{" "}
-                  </td>
-                  <td style={centerTransData}>
-                    <b>130.0</b>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td style={boldLabels} colSpan="6">
-                    Culmulative Grade Point Average:{" "}
-                  </td>
-                  <td style={centerTransData}>
-                    <b>3.79</b>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td colSpan="7">
-                    <b>Degree Conferred By:</b> Singapore University of Social
+								<tr>
+									<td colSpan="7">
+										<b>Degree Conferred By:</b> Singapore University of Social
                     Science{" "}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			)}
 
-      <div className="row">
-        <div className="col-4" style={boldLabels}>
-          Title
+			<div className="row">
+				<div className="col-4" style={boldLabels}>
+					Title
         </div>
-        <div className="col-4" style={boldLabels}>
-          Classification
+				<div className="col-4" style={boldLabels}>
+					Classification
         </div>
-        <div className="col-4" style={boldLabels}>
-          Date of Conferment
+				<div className="col-4" style={boldLabels}>
+					Date of Conferment
         </div>
-      </div>
+			</div>
 
-      <div className="row">
-        <div className="col-4">Bachelor of Building and Project Management</div>
-        <div className="col-4">Pass With Merit</div>
-        <div className="col-4">21 March 2019</div>
-      </div>
+			<div className="row">
+				<div className="col-4">{certificate.additionalData.degreeName2}</div>
+				<div className="col-4">
+					{certificate.additionalData.degreeClssification}
+				</div>
+				<div className="col-4">
+					{certificate.additionalData.dateOfConferment}
+				</div>
+			</div>
 
-      <div className="row">
-        <div className="col-4">
-          <img src={registrarSampleSign} />
-          <hr />
-          <div> Registrar </div>
-        </div>
-      </div>
+			<div className="row">
+				<div className="col-4">
+					<img src={certificate.additionalData.registraSignature} />
+					<hr />
+					<div> Registrar </div>
+				</div>
+			</div>
 
-      <div className="row">
-        <div className="col text-center">
-          --------------- End of Transcript ---------------
+			<div className="row">
+				<div className="col text-center">
+					--------------- End of Transcript ---------------
         </div>
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 Template.propTypes = {
-  certificate: PropTypes.object.isRequired
+	certificate: PropTypes.object.isRequired
 };
 
 export default Template;
